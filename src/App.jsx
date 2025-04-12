b74b369c00b5d2ba46431472ac2f61f55d9f2028
Anugrah <amvanugrah@gmail.com>
2025-04-12 22:59:32 +0530

    Done until Feature Cards Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Hero from "./sections/Hero.jsx"
+import ShowcaseSection from "./sections/ShowcaseSection.jsx"
+import LogoSection from "./sections/LogoSection.jsx"
+import FeatureCards from "./sections/FeatureCards.jsx"
+import Navbar from "./components/Navbar.jsx"
+
 
 const App = () => {
   return (
     <main>
        <>
+        <Navbar/>
         <Hero />
+        <ShowcaseSection/>
+        <LogoSection/>
+        <FeatureCards/>
        
        </>
       
