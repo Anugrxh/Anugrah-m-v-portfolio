879f083fa1c85bbcfefb5c5739abed983bface72
Anugrah <amvanugrah@gmail.com>
2025-04-14 19:15:08 +0530

    Experience section added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ShowcaseSection from "./sections/ShowcaseSection.jsx"
 import LogoSection from "./sections/LogoSection.jsx"
 import FeatureCards from "./sections/FeatureCards.jsx"
 import Navbar from "./components/Navbar.jsx"
+import ExperienceSection from "./sections/ExperienceSection.jsx"
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <ShowcaseSection/>
         <LogoSection/>
         <FeatureCards/>
+        <ExperienceSection/>
        
        </>
       
--- src/constants/index.js
+++ src/constants/index.js
@@ -148,8 +148,8 @@ const techStackIcons = [
 const expCards = [
   {
     review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-    imgPath: "/images/exp1.png",
-    logoPath: "/images/logo1.png",
+    imgPath: "/images/Croswer.png",
+    logoPath: "/images/CroswerLogo.png",
     title: "Frontend Developer",
     date: "January 2023 - Present",
     responsibilities: [
